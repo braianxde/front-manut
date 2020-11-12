b832cb0022c60ae4f40ab8786d40b29a58e0e96c
marciobecker <ml.becker@hotmail.com>
2020-11-12 09:34:32 -0300

    iniciando Dialog Criar chamado

--- src/components/CriarChamadoDialog.vue
+++ src/components/CriarChamadoDialog.vue
@@ -1,7 +1,25 @@
 <template>
-  <v-dialog v-model="dialogCriarChamado" max-width="500px">
+  <v-dialog v-model="dialogCriarChamado" max-width="600px"> 
     <v-card>
-      <span>{{ this.nomeUsuario }}</span>
+      <v-card-title class="blue White--text">
+        <span class="headline mb-1">Abrir Chamado</span>
+      </v-card-title>
+      <v-card class="mx-auto mb-2" max-width="580">
+        <v-card-text>
+          <p class="title sm-1 mb-1 text-left">Solicitante</p>
+          <p class="body-2 mb-1 text-left">Matricula: {{ matricula }}</p>
+          <p class="body-2 mb-1 text-left">Nome: {{ nome_solicitante }}</p>
+        </v-card-text>
+      </v-card>
+      <v-card class="mx-auto mb-2" max-width="580">
+        <v-card-text>
+          <p class="title sm-1 mb-1 text-left">Equipamento</p>
+          <v-text-field v-model="id_patrimonio" label="Nº Patrimônio" outlined></v-text-field>
+          <p class="body-2 mb-1 text-left">Nome: </p>
+          <p class="body-2 mb-1 text-left">Nome: </p>
+        </v-card-text>
+      </v-card>
+
     </v-card>
   </v-dialog>
 </template>
@@ -13,16 +31,23 @@ export default {
   name: "CriarChamadoDialog",
   props: {
     value: Boolean,
+    infonovochamado : Object
   },
-  created() {
-    this.nomeUsuario = Util.getInfoUsuario().nome;
-  },
-  data: () => ({
-    nomeUsuario: ""
+  data: () =>({
+    matricula: '',
+    nome_solicitante: '',
+    id_patrimonio:''
   }),
+
+  async created() {
+      this.matricula = (Util.getInfoUsuario().id);
+      this.nome_solicitante = (Util.getInfoUsuario().nome);
+    },
+
   computed: {
     dialogCriarChamado: {
       get() {
+        console.log(this.novochamado);
         return this.value;
       },
       set(value) {
@@ -30,7 +55,9 @@ export default {
       }
     }
   },
-  methods: {}
+  methods:{
+
+  }
 }
 </script>
 
